f79115edbb06c040c7d8b46bf8b6bf2fe93f834b
Matt Vella <mcvella@gmail.com>
2023-06-08 22:43:05 -0400

    fix requirements

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
+pip install -r ./requirements.txt
 cd `dirname $0`
-
 # Be sure to use `exec` so that termination signals reach the python process,
 # or handle forwarding termination signals manually
 exec python -m src.main $@
