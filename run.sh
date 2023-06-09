63de3bdd215db692d659c18d48cfde72132dd14a
Matt Vella <mcvella@gmail.com>
2023-06-08 21:01:37 -0400

    don't require poetry

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ cd `dirname $0`
 
 # Be sure to use `exec` so that termination signals reach the python process,
 # or handle forwarding termination signals manually
-exec poetry run python -m src.main $@
+exec python -m src.main $@
