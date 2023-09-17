c5888c20af4e525047a518109ccc34e98f876658
Matt Vella <mcvella@gmail.com>
2023-09-17 16:31:12 -0400

    change to python3

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 cd `dirname $0`
 # Be sure to use `exec` so that termination signals reach the python process,
 # or handle forwarding termination signals manually
-exec python -m src.main $@
+exec python3 -m src.main $@
