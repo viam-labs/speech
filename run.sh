6230ec5eae76b7c9238acd6d712ce8dd52261d11
Matt Vella <1242958+mcvella@users.noreply.github.com>
2023-09-22 09:57:17 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 cd `dirname $0`
+
+pip install --upgrade -r requirements.txt
+
 # Be sure to use `exec` so that termination signals reach the python process,
 # or handle forwarding termination signals manually
 exec python3 -m src.main $@
