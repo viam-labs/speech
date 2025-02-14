57684d4a73ee48beb47e1f50051bcd25f3af0f12
HipsterBrown <headhipster@hipsterbrown.com>
2025-02-14 13:26:21 -0500

    fix: use latest speech_service_api, viam-sdk releases

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ from viam.resource.registry import Registry, ResourceCreatorRegistration
 
 from src.speech.speechio import SpeechIOService, SpeechService
 
+
 async def main():
     """This function creates and starts a new module, after adding all desired resources.
     Resources must be pre-registered. For an example, see the `gizmo.__init__.py` file.
     """
-    Registry.register_resource_creator(SpeechService.SUBTYPE, SpeechIOService.MODEL, ResourceCreatorRegistration(SpeechIOService.new))
+    Registry.register_resource_creator(
+        SpeechService.API,
+        SpeechIOService.MODEL,
+        ResourceCreatorRegistration(SpeechIOService.new),
+    )
 
     module = Module.from_args()
-    module.add_model_from_registry(SpeechService.SUBTYPE, SpeechIOService.MODEL)
+    module.add_model_from_registry(SpeechService.API, SpeechIOService.MODEL)
     await module.start()
 
 
