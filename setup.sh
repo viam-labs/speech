143ed91dbeb17626fe5f5dadc8eb2a45272f7a1e
HipsterBrown <headhipster@hipsterbrown.com>
2025-11-17 10:28:43 -0500

    fix: adopt uv for python venv, dep install

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 cd `dirname $0`
 
 # Create a virtual environment to run our code
-VENV_NAME=$VIAM_MODULE_DATA/.venv
-PYTHON="$VENV_NAME/bin/python"
-ENV_ERROR="This module requires Python >=3.8, pip, and virtualenv to be installed."
+VIRTUAL_ENV=$VIAM_MODULE_DATA/.venv
+SUDO=sudo
+
+export PATH=$PATH:$HOME/.local/bin
+
+if ! command -v $SUDO; then
+  echo "no sudo on this system, proceeding as current user"
+  SUDO=""
+fi
 
 if command -v apt-get; then
   $SUDO apt-get install python3-pip git python3-pyaudio portaudio19-dev alsa-tools alsa-utils flac python3-dev build-essential -y
@@ -13,31 +19,21 @@ else
   echo "If you see failures below, install the equivalent of python3-venv for your system"
 fi
 
-if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
-    echo "Failed to create virtualenv."
-    if command -v apt-get >/dev/null; then
-        echo "Detected Debian/Ubuntu, attempting to install python3-venv automatically."
-        SUDO="sudo"
-        if ! command -v $SUDO >/dev/null; then
-            SUDO=""
-        fi
-		if ! apt info python3-venv >/dev/null 2>&1; then
-			echo "Package info not found, trying apt update"
-			$SUDO apt -qq update >/dev/null
-		fi
-        $SUDO apt install -qqy python3-venv >/dev/null 2>&1
-        if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
-            echo $ENV_ERROR >&2
-            exit 1
-        fi
-    else
-        echo $ENV_ERROR >&2
-        exit 1
-    fi
+if [ ! "$(command -v uv)" ]; then
+  if [ ! "$(command -v curl)" ]; then
+    echo "curl is required to install UV. please install curl on this system to continue."
+    exit 1
+  fi
+  echo "Installing uv command"
+  curl -LsSf https://astral.sh/uv/install.sh | sh
 fi
 
-# remove -U if viam-sdk should not be upgraded whenever possible
-# -qq suppresses extraneous output from pip
+if ! uv venv --allow-existing $VIRTUAL_ENV; then
+  echo "unable to create required virtual environment"
+  exit 1
+fi
+
+source $VIRTUAL_ENV/bin/activate
+
 echo "Virtualenv found/created. Installing/upgrading Python packages..."
-$PYTHON -m pip install --upgrade pip setuptools wheel -qq
-$PYTHON -m pip install -r requirements.txt -Uqq
+uv pip install -r requirements.txt -q
